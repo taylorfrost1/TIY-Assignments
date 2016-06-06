c873856ab33b6c04c54ed99ac8790cc7bc6f98f4
taylorfrost1 <taylorhamblinfrost@gmail.com>
2016-06-05 19:29:40 -0600

    completedJackPotApp

---
+++ 09 JSON App/jsonAppAssignment/jsonAppAssignment/MovieViewController.h
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.h
+//  jsonAppAssignment
+//
+//  Created by Taylor Frost on 6/3/16.
+//  Copyright © 2016 FrostEnterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Modal.h"
+
+@interface MovieViewController : UIViewController
+
+@property (nonatomic, strong) Modal *theMovie;
+
+@end
---
+++ 09 JSON App/jsonAppAssignment/jsonAppAssignment/MovieViewController.m
@@ -0,0 +1,40 @@
+//
+//  MovieViewController.m
+//  jsonAppAssignment
+//
+//  Created by Taylor Frost on 6/3/16.
+//  Copyright © 2016 FrostEnterprises. All rights reserved.
+//
+
+#import "MovieViewController.h"
+#import "Modal.h"
+#import "TableViewController.h"
+
+@interface MovieViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *nameOfMovieLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
+
+@end
+
+@implementation MovieViewController
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    self.nameOfMovieLabel.text = self.theMovie.originalTitle;
+    
+    NSLog(@"test for movie title generation");
+    
+    //This line could very well be wrong
+//self.posterImageView.image = [UIImage imageNamed:@"imageFromURLString"];
+    
+    
+    
+}
+    
+    
+    
+
+
+
+@end
