2ca68ec174affe585e00517d3ec742b865facb50
taylorfrost1 <taylorhamblinfrost@gmail.com>
2016-06-06 16:43:17 -0600

    startedIndianaJonesApp

---
+++ 10 Indiana Jones JSON App/indianaJonesProject/indianaJonesProject/IndyCharacter.h
@@ -0,0 +1,13 @@
+//
+//  IndyCharacter.h
+//  indianaJonesProject
+//
+//  Created by Taylor Frost on 6/6/16.
+//  Copyright © 2016 FrostEnterprises. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IndyCharacter : NSObject
+
+@end
---
+++ 10 Indiana Jones JSON App/indianaJonesProject/indianaJonesProject/IndyCharacter.m
@@ -0,0 +1,13 @@
+//
+//  IndyCharacter.m
+//  indianaJonesProject
+//
+//  Created by Taylor Frost on 6/6/16.
+//  Copyright © 2016 FrostEnterprises. All rights reserved.
+//
+
+#import "IndyCharacter.h"
+
+@implementation IndyCharacter
+
+@end
