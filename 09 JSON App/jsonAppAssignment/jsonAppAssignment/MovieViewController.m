ef13bc4db1315cb699a8ebf2bcaa2de0f077204f
taylorfrost1 <taylorhamblinfrost@gmail.com>
2016-06-06 10:58:12 -0600

    completedJsonAppAssignment

--- 09 JSON App/jsonAppAssignment/jsonAppAssignment/MovieViewController.m
+++ 09 JSON App/jsonAppAssignment/jsonAppAssignment/MovieViewController.m
@@ -27,15 +27,27 @@
     
     NSLog(@"test for movie title generation");
         
-//   UIImage *theImage = [self imageFromURLString: self.theMovie]
-    
-    
-    
+    UIImage *theImage = [self imageFromURLString: self.theMovie.posterPath];
+    self.posterImageView.image = theImage;
 }
-    
-    
-    
-
-
 }
+
+-(UIImage *)imageFromURLString:(NSString *)urlString {
+    
+    UIImage *theImage = nil;
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    NSLog(@"Attempting to Load urlString == %@", urlString);
+    
+    if(url) {
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        
+        if(data) {
+            theImage = [UIImage imageWithData:data];
+        }
+    }
+    return theImage;
+}
+
 @end
